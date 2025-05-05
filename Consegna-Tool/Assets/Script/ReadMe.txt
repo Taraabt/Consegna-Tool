c1c09a354e82fb958fe921b0f4bda27b71f87274
Alberto Tarabella <alberto.tarabella04@gmail.com>
2025-05-05 21:54:05 +0200

    v 1.0

---
+++ Consegna-Tool/Assets/Script/ReadMe.txt
@@ -0,0 +1,3 @@
+Per avviare il tool andare in Tools/BuildingTool
+Per istanziare il prefab premere invio
+Per ruotare  i prefab premere freccia su/freccia giu
--- Consegna-Tool/Assets/Script/Tool.cs
+++ Consegna-Tool/Assets/Script/Tool.cs
@@ -2,7 +2,9 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 using System;
-public class PrefabSpawner : EditorWindow
+using System.IO;
+
+public class BuildingTool : EditorWindow
 {
 
     Vector2 scrollviewpos; 
@@ -15,18 +17,20 @@ public class PrefabSpawner : EditorWindow
     bool canIstantiate = true;
     int layerMask=0;
     Material prefabMaterial;
+    string prefabName;
 
 
-    [MenuItem("Tools/Prefab Spawner Tool")]
+    [MenuItem("Tools/BuildingTool")]
     private static void ShowWindow()
     {
-        GetWindow<PrefabSpawner>("Prefab Spawner");
+        GetWindow<BuildingTool>("BuildingTool");
     }
 
     private void OnEnable()
     {
         searchbar = "";
-        sceneViewRect.size = new Vector2(300, 200);
+        prefabName = "";
+        sceneViewRect.size = new Vector2(200, 200);
         SceneView.duringSceneGui += DrawOnSceneGUI;
     }
 
@@ -47,11 +51,13 @@ public class PrefabSpawner : EditorWindow
             if (Physics.Raycast(ray, out hit, 1000, 1 << 0)&&!isSpawned)
             {
                 canIstantiate = true;
-                Debug.Log("Cursor Position in World: " + hit.point);
+                pos=hit.point;
                 Snap(hit);
-                pos.x = RoundFloat(hit.point.x);
-                pos.z= RoundFloat(hit.point.z);
-                instance.transform.position = new Vector3(pos.x, hit.point.y + instance.transform.localScale.y / 2, pos.z);
+                pos.x = RoundFloat(pos.x);
+                pos.z= RoundFloat(pos.z);
+                pos.y =RoundFloat( hit.point.y+instance.GetComponent<Collider>().bounds.size.y / 2);
+
+                instance.transform.position = new Vector3(pos.x, pos.y, pos.z);
                 pos = instance.transform.position;
                 instance.GetComponent<MeshRenderer>().material.color = Color.green;
             }
@@ -65,9 +71,19 @@ public class PrefabSpawner : EditorWindow
                     instance.GetComponent<MeshRenderer>().material.color = Color.red;
                     Vector3 worldPosition = ray.GetPoint(enter);
                     instance.transform.position = new Vector3(worldPosition.x, worldPosition.y + instance.transform.localScale.y / 2, worldPosition.z);
-                    Debug.Log("Posizione cursore nel mondo (senza hit): " + worldPosition);
+                    //Debug.Log("Posizione cursore nel mondo (senza hit): " + worldPosition);
                 }
             }
+        }
+
+        if (e.type == EventType.KeyDown && e.keyCode == KeyCode.UpArrow)
+        {
+            instance.transform.Rotate(0,90,0);
+        }
+
+        if(e.type == EventType.KeyDown && e.keyCode == KeyCode.DownArrow)
+        {
+            instance.transform.Rotate(90,0 , 0);
         }
 
         if (e.type == EventType.KeyDown && e.keyCode == KeyCode.Return&&canIstantiate)
@@ -76,9 +92,10 @@ public class PrefabSpawner : EditorWindow
             instance.transform.position = pos;
             instance.gameObject.layer=layerMask;
             instance.GetComponent<MeshRenderer>().material = prefabMaterial;
+            instance.tag = "Module";
         }
 
-        sceneViewRect = GUI.Window(0, sceneViewRect, DrawSceneViewRect, "Tool");
+        sceneViewRect = GUI.Window(0, sceneViewRect, DrawSceneViewRect, "BuildingTool");
         if (IsMouseOver() && Event.current.button == 0 && e.type == EventType.MouseDrag)
         {
             sceneViewRect.position += Event.current.delta;
@@ -87,10 +104,33 @@ public class PrefabSpawner : EditorWindow
         }
     }
 
-
     private void Snap(RaycastHit hit)
     {
+        GameObject[] module = GameObject.FindGameObjectsWithTag("Module");
+        for (int i = 0; i < module.Length; i++)
+        {
+            if (Vector3.Distance(module[i].transform.position, hit.point) <1.1f)
+            {
+                if (hit.point.x < 0)
+                {
+                    pos.x = module[i].transform.position.x - module[i].transform.GetComponent<Collider>().bounds.size.x;
+                }
+                else
+                {
+                    pos.x = module[i].transform.position.x + module[i].transform.GetComponent<Collider>().bounds.size.x;
+                }
+                if (hit.point.z < 0)
+                {
+                    pos.z = module[i].transform.position.z - module[i].transform.GetComponent<Collider>().bounds.size.z;
+                }
+                else
+                {
+                    pos.z = module[i].transform.position.z + module[i].transform.GetComponent<Collider>().bounds.size.z;
 
+                    return;
+                }
+            }
+        }
     }
 
     private float RoundFloat(float x)
@@ -98,7 +138,6 @@ public class PrefabSpawner : EditorWindow
         float parteDecimale = Mathf.Abs(x) - (float)Math.Floor(Mathf.Abs(x));
         int primaCifra = (int)(parteDecimale * 100) % 100 + 1;
         bool negative = false;
-        Debug.Log(primaCifra);
         if (x <= 0)
         {
             negative = true;
@@ -156,7 +195,7 @@ public class PrefabSpawner : EditorWindow
         list = GetPrefabs();
 
         searchbar = EditorGUILayout.TextField("Search: ", searchbar);
-        scrollviewpos = GUILayout.BeginScrollView(scrollviewpos, GUILayout.Height(150));
+        scrollviewpos = GUILayout.BeginScrollView(scrollviewpos, GUILayout.Height(100));
 
         for (int i = 0; i < list.Count; i++)
         {
@@ -164,17 +203,30 @@ public class PrefabSpawner : EditorWindow
                 continue;
             GUILayout.BeginHorizontal();
             Texture2D assetpreview = AssetPreview.GetAssetPreview(list[i]);
-            GUILayout.Label(assetpreview, GUILayout.Width(50), GUILayout.Height(50));
-            if (GUILayout.Button(list[i].name, GUILayout.Height(50))&&isSpawned)
+            GUILayout.Label(assetpreview, GUILayout.Width(50), GUILayout.Height(30));
+            if (GUILayout.Button(list[i].name, GUILayout.Height(30)))
             {
-                isSpawned = false;
-                instance = (GameObject)PrefabUtility.InstantiatePrefab(list[i]);
-                instance.transform.position = Vector3.zero;
-                prefabMaterial = instance.GetComponent<MeshRenderer>().sharedMaterial;
+                if (isSpawned)
+                {
+                    isSpawned = false;
+                    instance = (GameObject)PrefabUtility.InstantiatePrefab(list[i]);
+                    instance.transform.position = Vector3.zero;
+                    prefabMaterial = instance.GetComponent<MeshRenderer>().sharedMaterial;
+                }else if (!isSpawned)
+                {
+                    DestroyImmediate(instance);
+                    instance = (GameObject)PrefabUtility.InstantiatePrefab(list[i]);
+                    prefabMaterial = instance.GetComponent<MeshRenderer>().sharedMaterial;
+                }
             }
             GUILayout.EndHorizontal();
         }
         GUILayout.EndScrollView();
+        prefabName = EditorGUILayout.TextField("PrefabName: ", prefabName);
+        if (GUILayout.Button("SavePrefab", GUILayout.Width(190), GUILayout.Height(30)))
+        {
+            SavePrefab("Module");
+        }
     }
 
     private bool IsMouseOver()
@@ -203,6 +255,37 @@ public class PrefabSpawner : EditorWindow
             }
         }
         return gameobject;
+    }
+
+    private void SavePrefab(string tag)
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag(tag);
+        if (obj.Length == 0)
+        {
+            return;
+        }
+
+        GameObject parent = new GameObject(prefabName);
+
+        foreach (GameObject gameObject in obj)
+        {
+            gameObject.transform.SetParent(parent.transform);
+        }
+
+        string prefabFolder = "Assets/Save";
+        if (!Directory.Exists(prefabFolder))
+        {
+            Directory.CreateDirectory(prefabFolder);
+            AssetDatabase.Refresh();
+        }
+        if (prefabName=="")
+        {
+            prefabName = "NewPrefab";
+        }
+        string prefabPath = Path.Combine(prefabFolder, prefabName + ".prefab");
+        GameObject prefab = PrefabUtility.SaveAsPrefabAsset(parent, prefabPath);
+        DestroyImmediate(parent);
+        prefabName = "";
     }
 
 }
